8ad492b3ee71cbb021155c816e651ccaad5bacf6
freekmurze <freekmurze@users.noreply.github.com>
2023-04-01 15:04:04 +0000

    Fix styling

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -1,13 +1,9 @@
 <?php
 
-use PHPUnit\Framework\ExpectationFailedException;
-use Spatie\PestExpectations\Tests\TestSupport\LaravelRules\ValueShouldBeTrueRule;
-
-it('will only define the helpers when calling the register function', function() {
+it('will only define the helpers when calling the register function', function () {
     expect(function_exists('whenConfig'))->toBeFalse();
 
     registerSpatiePestHelpers();
 
     expect(function_exists('whenConfig'))->toBeTrue();
-
 });
