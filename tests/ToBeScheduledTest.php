4f321fd997c6a99cfac16d9e2f9243faa54e7b05
Enzo Innocenzi <enzo@innocenzi.dev>
2024-04-24 13:43:19 +0200

    test: remove `describe` usage

--- tests/ToBeScheduledTest.php
+++ tests/ToBeScheduledTest.php
@@ -7,50 +7,44 @@ use Spatie\PestExpectations\Tests\TestSupport\Scheduled\CommandClass;
 use Spatie\PestExpectations\Tests\TestSupport\Scheduled\InvokableClass;
 use Spatie\PestExpectations\Tests\TestSupport\Scheduled\JobClass;
 
-describe('call', function () {
-    it('can assert a class is scheduled by passing a cron expression', function () {
-        Schedule::call(InvokableClass::class)->hourly();
-        expect(InvokableClass::class)->toBeScheduled('0 * * * *');
-    });
+it('can assert a class is scheduled by passing a cron expression', function () {
+    Schedule::call(InvokableClass::class)->hourly();
+    expect(InvokableClass::class)->toBeScheduled('0 * * * *');
+});
 
-    it('can use a callback to assert anything on a scheduled class', function () {
-        Schedule::call(InvokableClass::class)->hourly();
+it('can use a callback to assert anything on a scheduled class', function () {
+    Schedule::call(InvokableClass::class)->hourly();
 
-        expect(InvokableClass::class)->toBeScheduled(function (CallbackEvent $event) {
-            expect($event->getExpression())->toBe('0 * * * *');
-            expect($event->getSummaryForDisplay())->toBe(InvokableClass::class);
-        });
+    expect(InvokableClass::class)->toBeScheduled(function (CallbackEvent $event) {
+        expect($event->getExpression())->toBe('0 * * * *');
+        expect($event->getSummaryForDisplay())->toBe(InvokableClass::class);
     });
 });
 
-describe('command', function () {
-    it('can assert a command is scheduled by passing a cron expression', function () {
-        Schedule::command(CommandClass::class)->daily();
-        expect(CommandClass::class)->toBeScheduled('0 0 * * *');
-    });
+it('can assert a command is scheduled by passing a cron expression', function () {
+    Schedule::command(CommandClass::class)->daily();
+    expect(CommandClass::class)->toBeScheduled('0 0 * * *');
+});
 
-    it('can use a callback to assert anything on a scheduled command', function () {
-        Schedule::command(CommandClass::class)->daily()->description('Foo');
+it('can use a callback to assert anything on a scheduled command', function () {
+    Schedule::command(CommandClass::class)->daily()->description('Foo');
 
-        expect(CommandClass::class)->toBeScheduled(function (Event $event) {
-            expect($event->getExpression())->toBe('0 0 * * *');
-            expect($event->getSummaryForDisplay())->toBe('Foo');
-        });
+    expect(CommandClass::class)->toBeScheduled(function (Event $event) {
+        expect($event->getExpression())->toBe('0 0 * * *');
+        expect($event->getSummaryForDisplay())->toBe('Foo');
     });
 });
 
-describe('job', function () {
-    it('can assert a job is scheduled by passing a cron expression', function () {
-        Schedule::job(JobClass::class)->daily();
-        expect(JobClass::class)->toBeScheduled('0 0 * * *');
-    });
+it('can assert a job is scheduled by passing a cron expression', function () {
+    Schedule::job(JobClass::class)->daily();
+    expect(JobClass::class)->toBeScheduled('0 0 * * *');
+});
 
-    it('can use a callback to assert anything on a scheduled job', function () {
-        Schedule::job(JobClass::class)->daily();
+it('can use a callback to assert anything on a scheduled job', function () {
+    Schedule::job(JobClass::class)->daily();
 
-        expect(JobClass::class)->toBeScheduled(function (Event $event) {
-            expect($event->getExpression())->toBe('0 0 * * *');
-            expect($event->getSummaryForDisplay())->toBe(JobClass::class);
-        });
+    expect(JobClass::class)->toBeScheduled(function (Event $event) {
+        expect($event->getExpression())->toBe('0 0 * * *');
+        expect($event->getSummaryForDisplay())->toBe(JobClass::class);
     });
 });
